e7a970035318d473abf4af1d34bedf4e1d1a2b29
Erikka Asem <missbees@live.com>
2021-03-04 21:01:09 -0600

    beginning of code

--- post-item.js
+++ post-item.js
@@ -10,3 +10,108 @@
 //neighborhood
 //image URL
 //post
+
+
+//the code below is from Kellogram and needs to be updated
+
+document.querySelector('form').addEventListener('submit', async function(event) {
+    event.preventDefault()
+    let postUsername = user.displayName
+    let postImageUrl = document.querySelector('#image-url').value
+    let response = await fetch('/.netlify/functions/create_post', {
+      method: 'POST',
+      body: JSON.stringify({
+        userId: user.uid,
+        username: postUsername,
+        imageUrl: postImageUrl
+      })
+    })
+    let post = await response.json()
+    document.querySelector('#image-url').value = '' // clear the image url field
+    renderPost(post)
+  })
+
+  let response = await fetch('/.netlify/functions/get_posts')
+  let posts = await response.json()
+  for (let i=0; i<posts.length; i++) {
+    let post = posts[i]
+    renderPost(post)
+  }
+} else {
+  // Signed out
+  console.log('signed out')
+
+  // Hide the form when signed-out
+  document.querySelector('form').classList.add('hidden')
+
+  // Initializes FirebaseUI Auth
+  let ui = new firebaseui.auth.AuthUI(firebase.auth())
+
+  // FirebaseUI configuration
+  let authUIConfig = {
+    signInOptions: [
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ],
+    signInSuccessUrl: 'index.html'
+  }
+
+  // Starts FirebaseUI Auth
+  ui.start('.sign-in-or-sign-out', authUIConfig)
+}
+})
+
+// given a single post Object, render the HTML and attach event listeners
+// expects an Object that looks similar to:
+// {
+//   id: 'abcdefg',
+//   username: 'brian',
+//   imageUrl: 'https://images.unsplash.com/...',
+//   likes: 12,
+//   comments: [
+//     { username: 'brian', text: 'i love tacos!' },
+//     { username: 'ben', text: 'fake news' }
+//   ]
+// }
+async function renderPost(post) {
+let postId = post.id
+document.querySelector('.posts').insertAdjacentHTML('beforeend', `
+  <div class="post-${postId} md:mt-16 mt-8 space-y-8">
+    <div class="md:mx-0 mx-4">
+      <span class="font-bold text-xl">${post.username}</span>
+    </div>
+    <div>
+      <img src="${post.imageUrl}" class="w-full">
+    </div>
+    <div class="text-3xl md:mx-0 mx-4">
+      <button class="like-button">❤️</button>
+      <span class="likes">${post.likes}</span>
+    </div>
+    <div class="comments text-sm md:mx-0 mx-4 space-y-2">
+      ${renderComments(post.comments)}
+    </div>
+    <div class="w-full md:mx-0 mx-4">
+      ${renderCommentForm()}
+    </div>
+  </div>
+`)
+
+// listen for the like button on this post
+let likeButton = document.querySelector(`.post-${postId} .like-button`)
+likeButton.addEventListener('click', async function(event) {
+  event.preventDefault()
+  console.log(`post ${postId} like button clicked!`)
+  let currentUserId = firebase.auth().currentUser.uid
+
+  let response = await fetch('/.netlify/functions/like', {
+    method: 'POST',
+    body: JSON.stringify({
+      postId: postId,
+      userId: currentUserId
+    })
+  })
+  if (response.ok) {
+    let existingNumberOfLikes = document.querySelector(`.post-${postId} .likes`).innerHTML
+    let newNumberOfLikes = parseInt(existingNumberOfLikes) + 1
+    document.querySelector(`.post-${postId} .likes`).innerHTML = newNumberOfLikes
+  }
+})
