d26a4372f3b0da59c6d1ca663fd2060ca7bc51e9
Erikka Asem <missbees@live.com>
2021-03-04 20:42:37 -0600

    EA outline

---
+++ post-item.js
@@ -0,0 +1,12 @@
+//This page is to post a new item
+//link to user profile
+//link to sign out
+//link to return to available items
+//site name
+//post new item
+//image name
+//item dimensions
+//color
+//neighborhood
+//image URL
+//post
